24ffade8b6291f551959d69039317a91f4bcf470
dsalenkov <salenkov.dd@phystech.edu>
2019-10-11 16:23:40 +0300

    ...

--- laba4/Laba4.py
+++ laba4/Laba4.py
@@ -11,45 +11,52 @@ class Game:
     def __init__(self):
         self.root = Tk()
         self.root.geometry('800x600')
-        self.canv = Canvas(self.root, bg='white')
-        self.canv.pack(fill=BOTH, expand=1)
+        self.canv = Canvas(self.root, bg = 'white')
+        self.canv.pack(fill = BOTH, expand = 1)
         self.ball = None
+        self.score = 0
 
 
     def click(self, event):
-        self.ball.click(event)
+        if self.ball.click(event) == 1:
+            self.score += self.ball.click(event)
+            print('Вы набрали ', self.score)
+        else:
+            print('Вы промахнулись')
+
 
 
     def new_ball(self):
         self.canv.delete(ALL)
-        self.ball = Ball(rnd(100, 700), rnd(100, 500), choice(colors), rnd(30, 40))
+        self.ball = Ball(rnd(100, 700), rnd(100, 500), choice(colors), rnd(30, 50))
         self.canv.create_oval(self.ball.cordx - self.ball.radius, self.ball.cordy - self.ball.radius,
                          self.ball.cordx + self.ball.radius, self.ball.cordy + self.ball.radius,
-                         fill=self.ball.color, width=0)
-        self.root.after(2000, self.new_ball)
+                         fill = self.ball.color, width = 0)
+        self.root.after(1000, self.new_ball)
         return self.ball
 
 
-class ObjectClick(Game):
+class Object(Game):
     def __init__(self, cordx, cordy, color):
         self.cordx = cordx
         self.cordy = cordy
         self.color = color
 
 
-class Ball(ObjectClick):
+class Ball(Object):
     def __init__(self, cordx, cordy, color, radius):
-        self.cordx = cordx
-        self.cordy = cordy
-        self.color = color
+        Object.__init__(self, cordx, cordy, color)
         self.radius = radius
 
 
     def click(self, event):
-        print(self.cordy, self.cordx)
-        print(event.x, event.y)
-        if ((self.cordx - event.x) ** 2) + ((self.cordy - event.x) ** 2) < self.radius ** 2 :
-            print('Попадание')
+        #print(self.cordx, self.cordy, self.radius)
+        #print(event.x, event.y)
+        if ((self.cordx - event.x) ** 2) + ((self.cordy - event.y) ** 2) <= self.radius ** 2 :
+            #print('Попадание')
+            return 1
+        else:
+            return 0
 
 
 
