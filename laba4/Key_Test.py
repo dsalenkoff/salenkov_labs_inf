42ce91f9cc227071edcda912729015aa2a5493f6
dsalenkov <salenkov.dd@phystech.edu>
2019-10-23 21:38:03 +0300

    Обновлено: добавил нормальное столкновение, поправил внешний вид

--- laba4/Key_Test.py
+++ laba4/Key_Test.py
@@ -1,9 +1,58 @@
-import tkinter as tk
+from random import randrange as rnd
 
-root = tk.Tk()
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-def key(event):
-    print(repr(event.char), repr(event.keysym), repr(event.keycode))
 
-root.bind("<Key>", key)
-root.mainloop()
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y + self.z * other.z
+        else:
+            return Vector(self.x * other, self.y * other, self.z * other)
+
+
+    def __abs__(self):
+        return ((self.x ** 2) + (self.y ** 2) + (self.z ** 2)) ** 0.5
+
+
+
+    def __getitem__(self, item):
+        if item == 0:
+            return self.x
+        elif item == 1:
+            return self.y
+        elif item == 2:
+            return self.z
+
+
+def len(self):
+        return ((self.x ** 2) + (self.y ** 2) + (self.z ** 2)) ** 0.5
+
+
+v1 = Vector(rnd(1, 10), rnd(1, 10), rnd(1, 10))
+print('Вектор 1: ', v1.x, v1.y, v1.z)
+v2 = Vector(rnd(1, 10), rnd(1, 10), rnd(1, 10))
+print('Вектор 2: ', v2.x, v2.y, v2.z)
+v3 = v1 + v2
+print('Вектор суммы: ', v3.x, v3.y, v3.z)
+v4 = v1 - v2
+print('Вектор разности: ', v4.x, v4.y, v4.z)
+scalar = v1 * v2
+print('Скалярное произведение векторов: ', scalar)
+k = rnd(1, 10)
+print('Коэффициент: ', k)
+v5 = v1 * k
+print('Вектор 1, умноженный на коэффициент: ', v5.x, v5.y, v5.z)
+print('Длина вектора 1: ', abs(v1))
+print('Длина вектора 1: ', len(v1))
--- laba4/Laba4.py
+++ laba4/Laba4.py
@@ -5,51 +5,37 @@ import math
 
 colors = ['#212638', '#072776', '#ba5a1a', '#fdb766', '#4b6700']
 
-N = 10 # Количество шариков
+N = 15
 
-x_pole = 1080 # cm
-y_pole = 720 # cm
+x_pole = 1080
+y_pole = 720
 
-min_velocity = 20 # cm/s
-max_velocity = 40 # cm/s
+min_velocity = 5
+max_velocity = 10
 
-max_radius = 40 # cm
-min_radius = 30 # cm
 
-density = 0.00093 # kg/cm^3
+min_radius = 20
+max_radius = 40
 
-red = 1.001
+red = 1
 
-global g
-
-g = 10 # cm/s^2
-dt = 5 # ms
+g = 30
+dt = 5
 
 class Game:
     def __init__(self):
         self.root = Tk()
         self.root.geometry(str(x_pole) + 'x' + str(y_pole))
-        self.canv = Canvas(self.root, bg = '#e9e9c6')
-        self.canv.pack(fill = BOTH, expand = 1)
+        self.canvas = Canvas(self.root, bg = '#e9e9c6')
+        self.canvas.pack(fill = BOTH, expand = 1)
         self.ball = None
         self.score = 0
         self.direction = 0
 
 
-    def click(self, event):
-        for i in balls:
-            if i.click(event, i) == 1:
-                self.score += i.click(event, i)
-                balls.append(Ball())
-                print('Вы набрали ', self.score)
-            else:
-                print('Вы промахнулись')
-
-
     def change(self, event):
         self.direction += 1
         self.direction = self.direction % 4
-        print(self.direction)
 
 
     def new_ball(self):
@@ -57,7 +43,7 @@ class Game:
         return self.ball
 
 
-class Object(Game):
+class Object:
     def __init__(self):
         self.image = None
         self.velocity_x = (rnd(min_velocity, max_velocity) - (min_velocity + max_velocity) / 2) / 4
@@ -71,46 +57,35 @@ class Ball(Object):
         self.radius = rnd(min_radius, max_radius)
         self.cord_x = rnd(max_radius, x_pole - max_radius)
         self.cord_y = rnd(max_radius, y_pole - max_radius)
-        for i in balls:
-            while (i.radius + self.radius) ** 2 >= (i.cord_x - self.cord_x) ** 2 + (i.cord_y - self.cord_y) ** 2:
+        for k in balls:
+            while (k.radius + self.radius) ** 2 >= (k.cord_x - self.cord_x) ** 2 + (k.cord_y - self.cord_y) ** 2:
                 self.cord_x = rnd(max_radius, x_pole - max_radius)
                 self.cord_y = rnd(max_radius, y_pole - max_radius)
         self.vector = None
-        self.mass = math.ceil(4.2 * (self.radius ** 3) * density)
-
-
-    def click(self, event, i):
-        #print(self.cord_x, self.cord_y, self.radius)
-        #print(event.x, event.y)
-        if ((i.cord_x - event.x) ** 2) + ((i.cord_y - event.y) ** 2) <= i.radius ** 2:
-            print(i.cord_x)
-            balls.remove(i)
-            return 1
-        else:
-            return 0
 
 
     def movement(self):
-        game.canv.delete(self.image)
-        game.canv.delete(self.vector)
-        self.image = game.canv.create_oval(self.cord_x - self.radius, self.cord_y - self.radius,
+        game.canvas.delete(self.image)
+        game.canvas.delete(self.vector)
+        self.image = game.canvas.create_oval(self.cord_x - self.radius, self.cord_y - self.radius,
                         self.cord_x + self.radius, self.cord_y + self.radius, fill = self.color,
                         width = 0)
-        self.vector = game.canv.create_line(self.cord_x, self.cord_y, self.cord_x + 20 * self.velocity_x,
-                                            self.cord_y + 20 * self.velocity_y, arrow = LAST, width = 5)
+        self.vector = game.canvas.create_line(self.cord_x, self.cord_y, self.cord_x + 15 * self.velocity_x,
+                                            self.cord_y + 15 * self.velocity_y, arrow = LAST, width = 5)
+        #self.collision()
         self.cord_x += self.velocity_x
         self.cord_y += self.velocity_y
         if game.direction == 0:
             self.velocity_x += 0
-            self.velocity_y += g * dt / 1000
+            self.velocity_y += g / 1000
         elif game.direction == 1:
-            self.velocity_x += g * dt / 1000
+            self.velocity_x += g / 1000
             self.velocity_y += 0
         elif game.direction == 2:
             self.velocity_x += 0
-            self.velocity_y -= g * dt / 1000
+            self.velocity_y -= g / 1000
         elif game.direction == 3:
-            self.velocity_x -= g * dt / 1000
+            self.velocity_x -= g / 1000
             self.velocity_y += 0
         self.collision()
         game.root.after(dt, self.movement)
@@ -118,47 +93,29 @@ class Ball(Object):
 
     def collision(self):
         if self.cord_x + self.radius >= x_pole:
-            self.velocity_x = -red*self.velocity_x
+            self.velocity_x = -red * self.velocity_x
             self.velocity_y = red * self.velocity_y
-        elif self.cord_x + self.radius >= x_pole + 2:
-            self.cord_x -= self.radius - 2
         elif self.cord_x - self.radius <= 0:
             self.velocity_x = -red * self.velocity_x
             self.velocity_y = red * self.velocity_y
-        elif self.cord_x - self.radius <= -2:
-            self.cord_x += self.radius + 2
         elif self.cord_y + self.radius >= y_pole:
-            self.velocity_y = -red*self.velocity_y
+            self.velocity_y = -red * self.velocity_y
             self.velocity_x = red * self.velocity_x
-        elif self.cord_y + self.radius >= y_pole + 2:
-            self.cord_y -= self.radius - 2
         elif self.cord_y - self.radius <= 0:
             self.velocity_y = -red * self.velocity_y
             self.velocity_x = red * self.velocity_x
-        elif self.cord_y - self.radius <= - 2:
-            self.cord_y += self.radius + 2
-
-        for i in balls:
-            #if self != i and (i.radius + self.radius) ** 2 + 10 >= (i.cord_x - self.cord_x) ** 2 + (i.cord_y - self.cord_y) ** 2:
-             #self.cord_x -= self.radius * ((((i.cord_x - self.cord_x) ** 2)/ ((i.radius + self.radius) ** 2)) ** 0.5)
-              #self.cord_y -= self.radius * ((((i.cord_y - self.cord_y) ** 2) / ((i.radius + self.radius) ** 2)) ** 0.5)
-            if self != i and (i.radius + self.radius) ** 2 >= (i.cord_x - self.cord_x) ** 2 + (i.cord_y - self.cord_y) ** 2:
-                self.velocity_x = -red * self.velocity_x
-                self.velocity_y = -red * self.velocity_y
-                #self.velocity_x = (self.mass * self.velocity_x - i.mass * (self.velocity_x - 2 * i.velocity_x)) / (self.mass + i.mass)
-                #self.velocity_y = (self.mass * self.velocity_y - i.mass * (self.velocity_y - 2 * i.velocity_y)) / (self.mass + i.mass)
+        for k in balls:
+            if self != k and (k.radius + self.radius) ** 2 >= (k.cord_x - self.cord_x) ** 2 + (k.cord_y - self.cord_y) ** 2:
+                velocity = (self.velocity_x ** 2 + self.velocity_y ** 2) ** 0.5
+                distance = ((k.cord_x - self.cord_x) ** 2 + (k.cord_y - self.cord_y) ** 2) ** 0.5
+                self.velocity_x = - red * velocity * ((k.cord_x - self.cord_x) / distance)
+                self.velocity_y = - red * velocity * ((k.cord_y - self.cord_y) / distance)
 
 game = Game()
-
 balls = []
-
 for i in range(N):
     balls.append(game.new_ball())
-
 for i in balls:
     i.movement()
-    print(i.mass, 'kg')
-
-game.canv.bind('<Button-1>', game.change)
-#game.canv.bind('<Button-1>', game.click)
+game.canvas.bind('<Button-1>', game.change)
 mainloop()
